94941b2c3472c6b120d4b1b55e31fabf1837067a
gokaydmrl <gokaydemirel96@gmail.com>
2022-09-29 23:05:31 +0300

    commit

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect } from "react";
 import { removeLoading } from "../redux/twitSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 
 const Loading = () => {
   const dispatch = useDispatch();
+  // const [warn, setWarn] = useState(1);
 
   // const handleClick = () => {
   //   /* do not forget to add pharanthesis
-  //   at the end of dispatch action :) */
+  //    at the end of dispatch action :) */
   //   dispatch(removeLoading());
   // };
+
 
   const data = useSelector((state) => state.twit.twits);
   const _loading = useSelector((state) => state.twit.loading);
@@ -22,17 +25,12 @@ const Loading = () => {
       }, 2000);
     timer();
 
-    return clearTimeout(timer);
+    return () => clearTimeout(timer);
   }, [data.length]);
 
   return (
     <div>
       {_loading && "Loading..."}
-      {/* {_loading ? (
-        <button onClick={handleClick}>anladım</button>
-      ) : (
-        "nö loading yet"
-      )} */}
     </div>
   );
 };
